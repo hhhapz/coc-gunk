d4bfeda438f5c8b3179e81223c5852fd8f4b3cf2
Hamza Ali <hamza.ali@brank.as>
2022-01-17 02:22:52 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# coc-gunk
+
+
+
+## Install
+
+`:CocInstall coc-gunk`
+
+## Keymaps
+
+`nmap <silent> <C-l> <Plug>(coc-coc-gunk-keymap)`
+
+## Lists
+
+`:CocList demo_list`
+
+## License
+
+MIT
+
+---
+
+> This extension is built with [create-coc-extension](https://github.com/fannheyward/create-coc-extension)
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import {ExtensionContext, LanguageClient, services, window} from 'coc.nvim';
+
+export async function activate(context: ExtensionContext): Promise<void> {
+  const client = new LanguageClient('coc-gunk', 'coc-gunk', {
+    command: 'gls',
+    // args: ['-pprof', '9999']
+  }, {
+    documentSelector: ['gunk'],
+    outputChannelName: 'coc-gunk',
+  })
+  context.subscriptions.push(services.registLanguageClient(client));
+}
