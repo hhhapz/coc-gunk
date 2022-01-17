6d37ea1a2498f03c098bed4c04f42cabbf5c9713
Hamza Ali <hamza.ali@brank.as>
2022-01-17 08:46:40 +0700

    update command from gls to gunkls

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import {ExtensionContext, LanguageClient, services, window} from 'coc.nvim';
 
 export async function activate(context: ExtensionContext): Promise<void> {
   const client = new LanguageClient('coc-gunk', 'coc-gunk', {
-    command: 'gls',
+    command: 'gunkls',
     // args: ['-pprof', '9999']
   }, {
     documentSelector: ['gunk'],
